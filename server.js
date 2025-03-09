ba675499559f01b3b29edf999d5e4ce9ccd5fb1c
deva <deva@ubuntu-vm.localdomain>
2025-03-09 15:05:17 +0530

    Updated Code

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
+// Bind to 0.0.0.0 instead of the default 127.0.0.1
+// This allows connections from outside the container
+const server = app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
 
